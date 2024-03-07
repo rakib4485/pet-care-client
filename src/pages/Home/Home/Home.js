c72b8e803f25ce7a8621f79818a7ee189b7ca6d9
xbayazid <bayazid9006@gmail.com>
2024-03-07 21:45:45 +0600

    home page sections file created

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Button } from "keep-react";
+import Hero from './Hero/Hero';
+import DogCards from './DogCards/DogCards';
+import About from './About/About';
+import Services from './Services/Services';
+import Shop from './Shop/Shop';
+import Features from '../Features/Features';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
-            <Button size="md">Default</Button>
+            <Hero/>
+            <DogCards/>
+            <About/>
+            <Services/>
+            <Shop/>
+            <Features/>
+            <Testimonial/>
         </div>
     );
 };
---
+++ src/pages/Home/Testimonial/Testimonial.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div>
+            <h1>Sabbir er kutta khob dusto. Pasher bashar beda kuttar er dick khaite chay</h1>
+        </div>
+    );
+};
+
+export default Testimonial;
