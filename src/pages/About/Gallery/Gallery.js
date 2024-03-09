0b7149a07df425a3fc4526d0118da42d0c110fee
xbayazid <bayazid9006@gmail.com>
2024-03-09 18:30:57 +0600

    awards section completed in about page

--- src/pages/About/Gallery/Gallery.js
+++ src/pages/About/Gallery/Gallery.js
@@ -11,15 +11,15 @@ const Gallery = () => {
           <div className='mx[8%]'>
           <h1 className='text-primary text-2xl font-bold text-center'>Photo Gallery</h1>
             <h1 className='choose-titles text-3xl lg:text-5xl font-bold text-center my-8'>Looking & Smelling Great!</h1>
-            <div className='flex justify-center items-center gap-5'>
+            <div className='lg:flex justify-center items-center gap-5'>
                 <div>
                     <img src={galleryOne} alt="" />
                     <img className='mt-5' src={galleryTwo} alt="" />
                 </div>
                 <div>
-                    <img className='rounded-2xl' src={galleryThree} alt="" />
+                    <img className='rounded-2xl hidden lg:block' src={galleryThree} alt="" />
                 </div>
-                <div>
+                <div className='mt-5 lg:mt-0'>
                     <img src={galleryFour} alt="" />
                     <img className='mt-5' src={galleryFive} alt="" />
                 </div>
