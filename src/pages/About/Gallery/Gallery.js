23bcbf2c9bbb212b1896002f159b6b58f7caa92a
xbayazid <bayazid9006@gmail.com>
2024-03-09 18:15:38 +0600

    gallery section completed on the about page

--- src/pages/About/Gallery/Gallery.js
+++ src/pages/About/Gallery/Gallery.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import galleryOne from '../../../assets/showcase-1.jpg';
+import galleryTwo from '../../../assets/showcase-4.jpg';
+import galleryThree from '../../../assets/showcase-2.jpg';
+import galleryFour from '../../../assets/showcase-3.jpg';
+import galleryFive from '../../../assets/showcase-5.jpg';
 
 const Gallery = () => {
     return (
-        <div>
-            <h1>Gallery</h1>
+        <div className='my-9 bg-gray-100 p-9'>
+          <div className='mx[8%]'>
+          <h1 className='text-primary text-2xl font-bold text-center'>Photo Gallery</h1>
+            <h1 className='choose-titles text-3xl lg:text-5xl font-bold text-center my-8'>Looking & Smelling Great!</h1>
+            <div className='flex justify-center items-center gap-5'>
+                <div>
+                    <img src={galleryOne} alt="" />
+                    <img className='mt-5' src={galleryTwo} alt="" />
+                </div>
+                <div>
+                    <img className='rounded-2xl' src={galleryThree} alt="" />
+                </div>
+                <div>
+                    <img src={galleryFour} alt="" />
+                    <img className='mt-5' src={galleryFive} alt="" />
+                </div>
+            </div>
+          </div>
         </div>
     );
 };
