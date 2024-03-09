5c2788dbf16692948636e43539846718c8ff2c03
rakib4485 <mdrakibulislam4485@gmail.com>
2024-03-09 15:46:12 +0600

    responsive done

--- src/pages/Home/Home/Hero/HeroCard.js
+++ src/pages/Home/Home/Hero/HeroCard.js
@@ -5,11 +5,11 @@ const HeroCard = ({item}) => {
     const {header,boldText1,boldText2,bgImg} = item;
     return (
         <div className='py-32' style={{backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
-            <div className='ml-24'>
-                <h4 className='text-[#FF4880] text-xl font-bold'>// <span className='mx-4'>{header}</span> //</h4>
-                <h1 className='text-7xl font-bold text-[#393D72]'>{boldText1}</h1>
-                <h1 className='text-7xl font-bold text-[#393D72]'>{boldText2}<span className='text-[#FF4880] text-8xl'>.</span></h1>
-                <div className='flex gap-4 mt-10'>
+            <div className='ml-10 md:ml-24'>
+                <h4 className='text-[#FF4880] text-lg md:text-xl font-bold'>// <span className='mx-4'>{header}</span> //</h4>
+                <h5 className='text-4xl md:text-7xl font-bold text-[#393D72]'>{boldText1}</h5>
+                <h4 className='text-4xl md:text-7xl font-bold text-[#393D72]'>{boldText2}<span className='text-[#FF4880] text-4xl md:text-8xl'>.</span></h4>
+                <div className='flex gap-4 mt-10 flex-col md:flex-row'>
                 <AppointmentButton baseColor={`bg-[#FF4880]`} HoverColor={`bg-[#8FC424]`} buttonText={`Make Appointment`}></AppointmentButton>
                 <AppointmentButton baseColor={`bg-[#8FC424]`} HoverColor={`bg-[#FF4880]`} buttonText={`+8801906283194`}></AppointmentButton>
                 </div>
